3bf7ef681641d6db1d6d1a2feae5fa4e37d8ca79
Aleix Riba <aleix.riba@cabsa.es>
2020-02-11 12:23:15 +0100

    create screen AddRestaurant and create button to navigate new screen

---
+++ app/screens/Restaurants/AddRestaurant.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+
+export default function AddRestaurant() {
+  return (
+    <View>
+      <Text>Add Restaurant...</Text>
+    </View>
+  );
+}
